37a408804fc22d6329c3ff10597bc9e6441b79d4
Dovydas Luksa <dluksa20@gmail.com>
2024-10-12 14:43:05 +0100

    final project

--- automating updation of catalog information/report_email.py
+++ automating updation of catalog information/report_email.py
@@ -1,34 +1,58 @@
 #!/usr/bin/env python3
 
-import email.message
-import mimetypes
-import os.path
-import smtplib
+import datetime
+import os
+from reports import generate_report
+from emails import generate_email, send_email
 
-def generate(sender, recipient, subject, body, attachment_path):
-  """Creates an email with an attachement."""
-  # Basic Email formatting
-  message = email.message.EmailMessage()
-  message["From"] = sender
-  message["To"] = recipient
-  message["Subject"] = subject
-  message.set_content(body)
 
-  # Process the attachment and add it to the email
-  attachment_filename = os.path.basename(attachment_path)
-  mime_type, _ = mimetypes.guess_type(attachment_path)
-  mime_type, mime_subtype = mime_type.split('/', 1)
+def pdf_body(input_for, desc_dir):
+    """Generating a summary with two lists, which gives the output name and weight"""
+    res = []    # names
+    wt = []     # weights
+    for item in os.listdir(desc_dir):
+        filename = os.path.join(desc_dir, item)
+        with open(filename) as f:
+            line = f.readlines()
+            weight = line[1].strip('\n')
+            name = line[0].strip('\n')
+            print(name, weight)
+            res.append('name: ' + name)
+            wt.append('weight: ' + weight)
+            print(res)
+            print(wt)
 
-  with open(attachment_path, 'rb') as ap:
-    message.add_attachment(ap.read(),
-                          maintype=mime_type,
-                          subtype=mime_subtype,
-                          filename=attachment_filename)
+    # Initialize the object
+    new_obj = ""
 
-  return message
+    # Calling values from two lists one by one.
+    for i in range(len(res)):
+        if res[i] and input_for == 'pdf':
+            new_obj += res[i] + '<br />' + wt[i] + '<br />' + '<br />'
+    return new_obj
 
-def send(message):
-  """Sends the message to the configured SMTP server."""
-  mail_server = smtplib.SMTP('localhost')
-  mail_server.send_message(message)
-  mail_server.quit()
+
+if __name__ == "__main__":
+    user = os.getenv('USER')
+    # The directory which contains all the files with data in it
+    description_directory = '/home/{}/supplier- data/descriptions/'.format(user)
+
+    # Creating data in format "May 5, 2020"
+    current_date = datetime.date.today().strftime("%B %d, %Y")
+
+    # Title for the PDF file with the created date
+    title = 'Processed Update on ' + str(current_date)
+
+    # Calling the report function from custom module
+    generate_report('/tmp/processed.pdf', title, pdf_body('pdf', description_directory))
+
+    # Subject line give in assignment for email
+    email_subject = 'Upload Completed - Online Fruit Store'
+
+    # Body line give in assignment for email
+    email_body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.'
+
+    # Structuring email and attaching the file. Then sending the email.
+    msg = generate_email("automation@example.com", "{}@example.com".format(user),
+                         email_subject, email_body, "/tmp/processed.pdf")
+    send_email(msg)
